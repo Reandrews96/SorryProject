322324d18cd292234d9daf4f20e6c432df2af9b7
yinawangg <ziwang@vassar.edu>
2019-04-19 19:02:21 -0400

    fix minimax

--- minimax.lisp
+++ minimax.lisp
@@ -20,34 +20,35 @@
 ;;  and the number of moves pruned during COMPUTE-MOVE.
 
 (defstruct stats
-  (num-moves 0)
-  (num-pruned 0))
+  (num-moves-done 0)
+  (num-potential-moves 0))
 
 ;;  COMPUTE-MOVE
 ;; -------------------------------------------------------------
-;;  INPUT:  G, a TTT struct
+;;  INPUT:  G, a sorry struct
 ;;          CUTOFF-DEPTH, depth at which eval func should be used
 ;;  OUTPUT:  The best move according to MINIMAX,
 ;;   using the static eval func, EVAL-FUNC.  Searches to
 ;;   a depth of *CUTOFF-DEPTH*.
 
 (defun compute-move (g cutoff-depth)
-  (let* ((me (ttt-whose-turn g))
-	 (game-over-results (game-over-score? g me))
-	 (statty (make-stats)))
     (cond
      ;; Case 1:  Game over...
-     (game-over-results
+     ((game-over g)
       (format t "Game is over: ~A!  Sorry dude!~%" game-over-results)
       nil)
      ;; Case 2:  Game still on, compute best move...
      (t
       ;; Call COMPUTE-MAX with init alpha/beta values
-      (let ((best-move (compute-max g 0 cutoff-depth statty)))
+      (let* ((statty (make-stats))
+	     (best-move nil));;(compute-max g 0 *neg-inf* *pos-inf* statty cutoff-depth)))
 	;; Report number of moves considered...
-	(format t "NUM-MOVES: ~A, PRUNED-MOVES: ~A~%" 
-		(stats-num-moves statty) (stats-num-pruned statty))
-	best-move)))))
+	(format t "NUM-MOVES-DONE: ~A, PRUNED-MOVES: ~A~%" 
+		(stats-num-moves-done statty) 
+		(- (stats-num-potential-moves statty)
+		   (stats-num-moves-done statty)))
+	(format t "BEST MOVE: ~A~%" best-move)
+	best-move))))
   
   
 ;;  COMPUTE-MAX / COMPUTE-MIN
@@ -62,27 +63,25 @@
 
 (defun compute-max (g curr-depth cutoff-depth statty)
   ;;(format t "COMPUTE-MAX:  cd=~A~%" curr-depth)
-  (let ((best-move-so-far nil)
-	(me (ttt-whose-turn g)))
+  (let ((best-move-so-far nil))
     (cond
      ;; Base Case 0:  Game over
-     ((game-over-score? g me)
+     ((game-over g)
       ;; just return that value:  either a LOSS or a DRAW
-      )
+      (+ *loss-value* curr-depth))
      ;; Base Case 1:  Game not over, but we're at the cutoff depth
      ((>= curr-depth cutoff-depth)
       ;; Use the static evaluation function: assumes game not over
       (eval-func g))     
      ;; Recursive Case:  Need to do minimax with alpha-beta pruning
      (t
-      (let* ((moves (legal-moves g))
-	     (num-moves-left (length moves))
-	     (curr-max *init-max-value*))
+      (let* ((moves (legal-moves g)))
+	(incf (stats-num-potential-moves statty) (length moves))
 	;;(format t "Compute MAX:  legal-moves: ~A~%" moves)
 	(dolist (mv moves)
 	  (incf (stats-num-moves statty))
 	  (decf num-moves-left)
-	  (do-move! g mv)
+	  (apply #'do-move! g nil mv)
 	  (let* ((index (position mv moves))
 		 (prob (/ (svref (sorry-deck g) index) (sorry-num-cards g)))
 		 (child-val (* prob (compute-min g (1+ curr-depth) cutoff-depth statty))))
@@ -91,7 +90,14 @@
 	    (when (> child-val curr-max)
 	      (setf curr-max child-val)
 	      (setf best-move-so-far mv)
-	      )))
+	      (when (<= beta alpha)
+		(return-from compute-max
+		  (cond
+		   ((zerop curr-depth)
+		    (format t "     ROOT NODE ALPHA: ~A~%" alpha)
+		    best-move-so-far)
+		   (t
+		    alpha)))))))
 	;; return curr-max or best-move-so-far, depending on whether
 	;; we're at depth 0 or not
 	(cond
@@ -103,7 +109,7 @@
 
 ;;  COMPUTE-MIN
 ;; -------------------------------------------------------
-;;  INPUTS:  G, a CHESS struct
+;;  INPUTS:  G, a SORRY struct
 ;;           CURR-DEPTH, the depth of this MIN node
 ;;           CUTOFF-DEPTH, depth at which eval func should be used
 ;;           STATTY, a STATS struct
@@ -111,13 +117,12 @@
 ;;           of MINIMAX with ALPHA-BETA pruning
 
 (defun compute-min (g curr-depth cutoff-depth statty)
-  ;;(format t "COMPUTE-MIN:  cd=~A, alpha=~A, beta=~A~%" curr-depth alpha beta)
-  (let ((me (other-player (ttt-whose-turn g)))) 
+  ;;(format t "COMPUTE-MIN:  cd=~A, alpha=~A, beta=~A~%" curr-depth alpha beta) 
     (cond
      ;; Base Case 0: Game over... and score computed
-     ((game-over-score? g me)
+     ((game-over g)
       ;; just return that value: either a WIN or a DRAW
-      )
+      (- *win-value* curr-depth))
      ;; Base Case 1:  Game not over, but we're at the cutoff depth
      ((>= curr-depth cutoff-depth)
       ;; Let static eval func do its thing: assumes game not over
@@ -125,20 +130,21 @@
      
      ;; Otherwise, we need to use recursion!
      (t
-      (let* ((moves (legal-moves g))  
-	     (num-moves-left (length moves))
-	     (curr-min *init-min-value*))
+      (let* ((moves (legal-moves g)))
+	(incf (stats-num-potential-moves statty) (length moves))
 	;;(format t "Compute MIN:  legal-moves: ~A~%" moves)
 	(dolist (mv moves)
 	  (incf (stats-num-moves statty))
 	  (decf num-moves-left)
-	  (do-move! g mv)
+	  (apply #'do-move! g nil mv)
 	  (let* ((index (position mv moves))
 		 (prob (/ (svref (sorry-deck g) index) (sorry-num-cards g)))
 		 (child-val (* prob (compute-max g (1+ curr-depth) cutoff-depth statty))))
 	    (undo-move! g)
-	    ;; Update CURR-MIN value if necessary...
-	    (setf curr-min (min curr-min child-val))))
-	;; return curr-min 
+	    (when (< child-val beta)
+	      (setf beta child-val)
+	      (when (<= beta alpha)
+		(return-from compute-min beta)))))
+	;; return beta
 	;;  NOTE:  Depth can't be zero for a MIN node
-	curr-min)))))
+	beta))))
